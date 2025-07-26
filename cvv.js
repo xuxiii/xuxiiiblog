3a9520af539cf8c37c59258fe8333dafbccfd7f5
xuxiii <dotnkdot@gmail.com>
2025-07-26 19:04:25 +0300

    upd

---
+++ cvv.js
@@ -0,0 +1,3 @@
+    document.getElementById("cvv").addEventListener("play", function() {
+        alert("с вашей карты уже списано 500 рублей за прослушивание молодой трэп миллионер за работой");
+    });
---
+++ lastupd.js
@@ -0,0 +1,4 @@
+    function myFunction() {
+      var x = document.lastModified;
+      document.getElementById("demo").innerHTML = x;
+    }
